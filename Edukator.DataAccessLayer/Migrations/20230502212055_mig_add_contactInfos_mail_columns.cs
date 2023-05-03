790cada04d5b569f32656a7eaa733e0def750508
kkele <kkele@DESKTOP-MM84V2T>
2023-05-03 22:03:13 +0300

    Repository yapısı oluşturuldu

---
+++ Edukator.DataAccessLayer/Migrations/20230502212055_mig_add_contactInfos_mail_columns.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Edukator.DataAccessLayer.Migrations
+{
+    public partial class mig_add_contactInfos_mail_columns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Mail1",
+                table: "ContactInfos",
+                type: "nvarchar(50)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Mail2",
+                table: "ContactInfos",
+                type: "nvarchar(60)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mail1",
+                table: "ContactInfos");
+
+            migrationBuilder.DropColumn(
+                name: "Mail2",
+                table: "ContactInfos");
+        }
+    }
+}
